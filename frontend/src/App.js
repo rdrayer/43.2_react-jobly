041b4c51934702528c13debb1c52a1fc986a3ef9
Roberta Drayer <robertadrayer@gmail.com>
2024-03-04 22:52:53 -0800

    initial commit/addedfr end routes

---
+++ frontend/src/App.js
@@ -0,0 +1,48 @@
+import React , { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import './App.css';
+import JoblyApi from "./api";
+import Home from "./Home";
+//import NavBar from "./NavBar";
+//import CompaniesList from
+//import CompanyDetails from
+//import JobsList from
+//import Login from
+//import Profile from
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            {/*
+            <Route path="/companies">
+              <CompaniesList />
+            </Route>
+            <Route path="/companies/:companyId">
+              <CompanyDetails />
+            </Route>
+            <Route path="/jobs">
+              <JobsList />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/signup">
+              <SignUp />
+            </Route>
+            <Route path="/profile">
+              <Profile />
+            </Route>
+            */}
+            <Route element={<p>Hmm, I can't seem to find the page you're looking for.</p>} />
+          </Routes>     
+        </main>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
